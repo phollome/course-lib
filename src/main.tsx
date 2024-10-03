eeb58ad790034a01cbe959b99c18a862655bd00e
Peter Holló <hello@songsforthe.dev>
2024-10-03 15:11:08 +0200

    refactor: Root element null check

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,15 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import App from './App.tsx'
-import './index.css'
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./index.css";
 
-createRoot(document.getElementById('root')!).render(
+const root = document.getElementById("root");
+if (root === null) {
+  throw new Error("No root element found");
+}
+
+createRoot(root).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
